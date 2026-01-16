cedc2fc2a6f503d606429451df7f50a81d2b436a
Suraj Chaudhary <isurajcdry@gmail.com>
2026-01-16 10:46:17 -0500

    using webpack for mdx support

--- app/blog/layout.tsx
+++ app/blog/layout.tsx
@@ -1,32 +1,5 @@
-'use client'
-import { TextMorph } from '@/components/ui/text-morph'
 import { ScrollProgress } from '@/components/ui/scroll-progress'
-import { useEffect, useState } from 'react'
-
-function CopyButton() {
-  const [text, setText] = useState('Copy')
-  const currentUrl = typeof window !== 'undefined' ? window.location.href : ''
-
-  useEffect(() => {
-    setTimeout(() => {
-      setText('Copy')
-    }, 2000)
-  }, [text])
-
-  return (
-    <button
-      onClick={() => {
-        setText('Copied')
-        navigator.clipboard.writeText(currentUrl)
-      }}
-      className="font-base flex items-center gap-1 text-center text-sm text-zinc-500 transition-colors dark:text-zinc-400"
-      type="button"
-    >
-      <TextMorph>{text}</TextMorph>
-      <span>URL</span>
-    </button>
-  )
-}
+import { CopyButton } from './copy-button'
 
 export default function LayoutBlogPost({
   children,
--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,6 @@ import createMDX from '@next/mdx';
 const nextConfig = {
   reactStrictMode: true,
   pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'md', 'mdx'],
-  turbopack: {},
 };
 
 const withMDX = createMDX({
